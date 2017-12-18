a40c02c059d63ad286b0791bea8b9a50590974f2
Dale Nguyen <dungnq@itbox4vn.com>
2017-12-18 12:16:18 -0500

    Added Signin and Signup Controller

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('bucketList', ['ionic', 'firebase'])
+angular.module('bucketList', ['ionic', 'firebase', 'bucketList.controllers'])
 
 .run(function($ionicPlatform, $rootScope, $firebaseAuth, $firebase, $window, $ionicLoading) {
   $ionicPlatform.ready(function() {
