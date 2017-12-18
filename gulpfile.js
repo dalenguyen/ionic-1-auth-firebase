4fe7345f7a27939a75012a6079d5ad34d9804053
Dale Nguyen <dungnq@itbox4vn.com>
2017-12-18 10:50:05 -0500

    Setup gulp serve with gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
 var sass = require('gulp-sass');
-var cleanCss = require('gulp-clean-css');
+var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var sh = require('shelljs');
+var connect = require('gulp-connect');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  www : ['www/**/*.*']
 };
 
 gulp.task('default', ['sass']);
+gulp.task('serve', ['connect', 'watch']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
-    .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
-    .pipe(cleanCss({
+    .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
@@ -22,6 +28,42 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
-gulp.task('watch', ['sass'], function() {
-  gulp.watch(paths.sass, ['sass']);
+gulp.task('reload', function () {
+  return gulp.src(['www/index.html'])
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  // Uncomment below line if you wish to work with SASS
+  //gulp.watch(paths.sass, ['sass']);
+
+  gulp.watch([paths.www], ['reload']);
+});
+
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'www',
+    port: '1881',
+    livereload: true
+  });
 });
